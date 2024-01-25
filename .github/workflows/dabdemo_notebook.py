4d0ba85bf3c3f744eb99c4bda6f6429f462d77c5
satish916560 <156743252+satish916560@users.noreply.github.com>
2024-01-25 13:32:59 +0530

    Update dabdemo_notebook.py

--- .github/workflows/dabdemo_notebook.py
+++ .github/workflows/dabdemo_notebook.py
@@ -3,23 +3,10 @@
 # COMMAND ----------
 
 # Restart Python after installing the wheel.
-dbutils.library.restartPython()
 
 # COMMAND ----------
 
-from dabdemo.addcol import with_status
 
-df = (spark.createDataFrame(
-  schema = ["first_name", "last_name", "email"],
-  data = [
-    ("paula", "white", "paula.white@example.com"),
-    ("john", "baer", "john.baer@example.com")
-  ]
-))
-
-new_df = with_status(df)
-
-display(new_df)
 
 # Expected output:
 #
